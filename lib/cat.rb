cadb45ca78820f3b9eaf83e0e7733c598d54dd3c
Teerth Patel <teerth1205@gmail.com>
2020-02-06 20:11:55 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,10 @@
 class Cat
   # code goes here
+  attr_accessor :mood,:owner
+  attr_reader :name
+
+  def initialize(name,owner)
+    @name = name
+     @owner = Owner.new(owner)
+  end
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,6 @@
 class Owner
-  # code goes here
+  attr_accessor :name 
+def initialize(name)
+  @name = name 
+end
 end
